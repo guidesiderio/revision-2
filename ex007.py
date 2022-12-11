d27e59e08b9d6186d8d39c34884c160710b4015e
Guilherme Desidério <guilhermedesiderio23@gmail.com>
2022-12-11 18:09:40 -0300

    Maior número

---
+++ ex007.py
@@ -0,0 +1,9 @@
+maior = 0
+for i in range(1, 6):
+    ler_valor = input('Digite um valor: ')
+    valor = float(ler_valor)
+    if valor >= maior:
+        maior = valor
+
+print(f'Maior = {maior}')  
+
