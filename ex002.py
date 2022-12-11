e82180397ac4e6b960aaa289fd87de14d26ef49f
Guilherme Desidério <guilhermedesiderio23@gmail.com>
2022-12-11 14:43:11 -0300

    Nome e senha

---
+++ ex002.py
@@ -0,0 +1,12 @@
+print('Login:')
+
+nome_usuario = input('Digite o seu nome de usuário: ')
+senha = input('Digite a sua senha: ')
+
+while nome_usuario == senha:
+    print('Error login! Nome de usuário igual senha')
+    nome_usuario = input('Digite o seu nome de usuário: ')
+    senha = input('Digite a sua senha: ')
+
+print('Login feito com sucesso!')
+
