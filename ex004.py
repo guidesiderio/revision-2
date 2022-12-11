0f7d1d8ea41d6b7dc3f5398b1b9e69441a8648f6
Guilherme Desidério <guilhermedesiderio23@gmail.com>
2022-12-11 16:09:16 -0300

    Taxa de crescimento

---
+++ ex004.py
@@ -0,0 +1,15 @@
+populacao_a = 80000
+taxa_crescimento_a = 0.03
+
+populacao_b = 200000
+taxa_crescimento_b = 0.015
+
+anos = 0
+
+while populacao_a <= populacao_b:
+    populacao_a = populacao_a + (populacao_a * taxa_crescimento_a)
+    populacao_b = populacao_b + (populacao_b * taxa_crescimento_b)
+    anos += 1
+
+print(f'É necessário {anos} anos para que a população "A" ultrapasse a população "B".') 
+
