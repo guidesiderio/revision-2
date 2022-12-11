c5e79275bf87d788636de5ee12632b634fe88d40
Guilherme Desidério <guilhermedesiderio23@gmail.com>
2022-12-11 18:09:13 -0300

    Soma e Média

---
+++ ex008.py
@@ -0,0 +1,10 @@
+soma = 0
+media = 0
+for i in range(1, 6):
+    ler_valor = input('Digite um valor: ')
+    valor = float(ler_valor)
+    soma += valor
+    media = soma / i
+
+print(f'Soma = {soma}, Media = {media:.2f}')   
+
